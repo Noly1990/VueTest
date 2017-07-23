33e4ee2e767fa7b1e78805ad43ef0153f20a598d
Noly1990 <715285974@qq.com>
2017-07-23 16:27:21 +0800

    to test vue-router

--- js/app.js
+++ js/app.js
@@ -54,15 +54,38 @@ new Vue({
 });
 
 
-Vue.http.options.headers = { "X-Mashape-Key": "OivH71yd3tmshl9YKzFH7BTzBVRQp1RaKLajsnafgL2aPsfP9V", "Accept": "application/json", "Content-type": "application/x-www-form-urlencoded" };
+//test vue-resource 功能
 
-Vue.http.post("https://andruxnet-random-famous-quotes.p.mashape.com/cat=", { "fname": "Yan", "lname": "Jun" }, { emulateJSON: true }).then(
-    function(res) {
-        console.log('success');
-        console.log(res.body);
-    },
-    function() {
-        console.log('error');
-    });
+// Vue.http.options.headers = { "X-Mashape-Key": "OivH71yd3tmshl9YKzFH7BTzBVRQp1RaKLajsnafgL2aPsfP9V", "Accept": "application/json", "Content-type": "application/x-www-form-urlencoded" };
+// Vue.http.post("https://andruxnet-random-famous-quotes.p.mashape.com/cat=", { "fname": "Yan", "lname": "Jun" }, { emulateJSON: true }).then(
+//     function(res) {
+//         console.log('success');
+//         console.log(res.body);
+//     },
+//     function() {
+//         console.log('error');
+//     });
 
 //import myCpnt from './myCpnt.vue'
+
+
+
+//test vue-router
+const aaa={template:'<p>I am aaa component</p>'};
+const bbb={template:'<div>I am bbb component</div>'};
+
+const routes=[
+    {path:'/aaa',
+    component:aaa
+
+    },{
+path:'/bbb',
+    component:bbb
+    }
+];
+
+const router=new VueRouter({
+    routes
+});
+
+const routertest=new Vue({router}).$mount("#routertest");
